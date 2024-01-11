f941991ef57df35fedf80f683ddfb946d27f7065
KorotunYura <yurakorotun1991@gmail.com>
2024-01-11 09:49:12 +0200

    task-3(promises)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const selectors = {
+  form: document.querySelector('.form'),
+};
+
+selectors.form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  let firstDelay = Number(selectors.form.elements.delay.value);
+  const stepDelay = Number(selectors.form.elements.step.value);
+  const amount = Number(selectors.form.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    firstDelay += stepDelay;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
